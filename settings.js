e6bb4b11a1a2f93b0d61ace7cc749be1a42e4f63
Opulent <78411012+Opulent54@users.noreply.github.com>
2025-05-30 21:42:59 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Sumit', // Your name
-  ownerNumber: '6282122664060', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '916009512884', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
